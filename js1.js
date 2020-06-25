952445611951fecba7d56689f33a6269a3af0f09
Victor Cheng <victorcheng0425@gmail.com>
2020-06-24 22:10:08 -0700

    testing

---
+++ js1.js
@@ -0,0 +1,10 @@
+
+
+//alert("Get out~");
+console.log('Testing the javascript file!');
+function sayHello() {
+    document.write("Hello, World!!!!!!!!!!!!!!!!!!!!!!");
+    alert("Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! Get out!! ");
+    //alert("Get out!! Get out!! ");
+ }
+ sayHello();
--- js_helloworld.js
+++
@@ -1,5 +0,0 @@
-console.log('Testing the javascript file!');
-function sayHello() {
-    document.write("Hello, World!");
- }
- sayHello();
